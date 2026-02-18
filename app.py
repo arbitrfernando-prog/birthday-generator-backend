35f92b7e24c1c645ea97f58093fbceee1025031d
arbitrfernando-prog <arbitrfernando@gmail.com>
2026-02-18 16:25:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+import sys
+import logging
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+print("=== Starting Flask app ===", flush=True)
+
 import os
 import json
 from flask import Flask, request, jsonify
